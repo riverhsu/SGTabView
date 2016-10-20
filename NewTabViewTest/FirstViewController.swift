68bab83c3c50888df9563dcacdfa23ffd06d2724
Sgmedical <sgmedical@sgs-macbook-air.local>
2016-10-20 17:26:51 +0800

    Initial Commit

---
+++ NewTabViewTest/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  NewTabViewTest
+//
+//  Created by Sgmedical on 2016/10/20.
+//  Copyright © 2016年 Sgmedical. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
